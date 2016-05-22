6b3c32aab4b9d08ac241a5a944327d1ee01dfbc7
Li Zheng <lixiaozheng.good@163.com>
2016-05-22 22:49:46 +0800

    Shudu

---
+++ app/src/main/java/com/example/a10648/shudu/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.a10648.shudu;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(new ShuduView(this));
+    }
+}
